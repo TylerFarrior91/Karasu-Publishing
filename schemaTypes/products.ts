ed11155e601323ba18044613eebf6a5ea342a12a
Tyler Farrior <tylerfarrior@gmail.com>
2024-03-29 06:45:47 -0400

    adding product code to products.ts

--- schemaTypes/products.ts
+++ schemaTypes/products.ts
@@ -6,6 +6,23 @@ export default {
 {
     name: 'name',
     type: 'string',
+    title: 'Name of Product'
+},
+{
+    name: 'images',
+    type: 'array',
+    title: 'Product Images',
+    of: [{type: 'image'}],
+},
+{
+    name: 'description',
+    type: 'text',
+    title: 'Description of Product',
+},
+{
+    name: 'slug',
+    type: 'slug',
+    title: 'Product Slug',
 }
     ]
 }
