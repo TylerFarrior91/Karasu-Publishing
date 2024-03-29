165c50340b79c5c82d207ddca797b90acd9688c0
Tyler Farrior <tylerfarrior@gmail.com>
2024-03-29 06:48:21 -0400

    finishing up products.ts file

--- schemaTypes/products.ts
+++ schemaTypes/products.ts
@@ -3,26 +3,31 @@ export default {
     type: 'document',
     title: 'Product',
     fields: [
-{
-    name: 'name',
-    type: 'string',
-    title: 'Name of Product'
-},
-{
-    name: 'images',
-    type: 'array',
-    title: 'Product Images',
-    of: [{type: 'image'}],
-},
-{
-    name: 'description',
-    type: 'text',
-    title: 'Description of Product',
-},
-{
-    name: 'slug',
-    type: 'slug',
-    title: 'Product Slug',
-}
-    ]
+        {
+            name: 'name',
+            type: 'string',
+            title: 'Name of Product'
+        },
+        {
+            name: 'images',
+            type: 'array',
+            title: 'Product Images',
+            of: [{ type: 'image' }],
+        },
+        {
+            name: 'description',
+            type: 'text',
+            title: 'Description of Product',
+        },
+        {
+            name: 'slug',
+            type: 'slug',
+            title: 'Product Slug',
+        },
+        {
+            name: 'price',
+            type: 'Price',
+            title: 'number',
+        },
+    ],
 }
