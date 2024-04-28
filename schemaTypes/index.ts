d0bf60b6be8f09d92103edf99b6ca4fd5ed8366a
Tyler Farrior <tylerfarrior@gmail.com>
2024-04-28 09:46:14 -0400

    adding to heroimages.ts and index.ts

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,4 +1,5 @@
 import category from "./category";
+import heroimages from "./heroimages";
 import products from "./products";
 
-export const schemaTypes = [products, category]
+export const schemaTypes = [products, category, heroimages]
